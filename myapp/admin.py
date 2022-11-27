cbb3aba737d351ac1ea9f7dca986246f940c0556
Gayan Kuruppu <gayan.kuruppu@live.com>
2022-11-27 13:06:07 +0000

    Set up admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 from .models import Airplane
+from .models import Employee
+from .models import Passenger
 
 # Register your models here.
 
+
 admin.site.register(Airplane)
+admin.site.register(Employee)
+admin.site.register(Passenger)
+admin.site.unregister(Group)
+admin.site.unregister(User)
--- myapp/models.py
+++ myapp/models.py
@@ -10,8 +10,40 @@ class Airplane(models.Model):
     sub_model_name = models.CharField(max_length=20)
 
     def __str__(self):
-        return "%s %s %s %s" (self.airplane_number, self.manufacturer_name, self.model_name, self.sub_model_name)
+        return self.airplane_number
 
+
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    job_name = models.CharField(max_length=10)
+    surname = models.CharField(max_length=45)
+    other_names = models.CharField(max_length=45)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.job_name + ' ' + self.surname + ' ' + self.other_names
+
+
+class Passenger(models.Model):
+    passenger_id = models.AutoField(primary_key=True)
+    passport_no = models.CharField(max_length=10)
+    type = models.CharField(max_length=45)
+    country_code = models.CharField(max_length=45)
+    surname = models.CharField(max_length=200)
+    other_names = models.CharField(max_length=20)
+    national_status = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    id_no = models.IntegerField()
+    profession = models.CharField(max_length=20)
+    sex = models.BooleanField()
+    place_of_birth = models.CharField(max_length=20)
+    date_of_issue = models.DateField()
+    date_of_expiry = models.DateField()
+    authority = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.passport_no
 
 # class Article(models.Model):
 #     headline = models.CharField(max_length=100)
